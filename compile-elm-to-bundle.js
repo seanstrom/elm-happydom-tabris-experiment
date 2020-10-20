e6836099db6f6d6a1e472d63d202d6874acabba5
Sean Hagstrom <sean@usejournal.com>
2020-10-19 17:36:09 -0700

    update gitignore, move elm-dist folder, tweak elm build script

--- compile-elm-to-bundle.js
+++ compile-elm-to-bundle.js
@@ -1,16 +1,27 @@
 const fs = require('fs/promises')
 const { compileToString } = require('node-elm-compiler');
 
+/**
+ * Note: We save the compilation into the `src` directory to avoid unusual
+ * behavior from the `tabris` server. Attempting to move it the bundle file may
+ * silently break the server, causing the app not to update.
+ */
 compileToString(
   ['./src/Main.elm'],
   { pathToElm: './node_modules/.bin/elm' }
 ).then(data => {
   const wrappedCode = wrapElmCode(data);
-  return fs.writeFile('./elm-dist/bundle.js', wrappedCode);
+  return fs.writeFile('./src/elm-dist/elm-bundle.js', wrappedCode);
 }).then(() => {
-  console.log('Done compiling Elm code')
+  console.log('Done compiling Elm code');
+}).catch(() => {
+  console.log('Oops cannot compile Elm code');
 });
 
+/**
+ * Note: We needed to create a function wrapper around the compiled JavaScript
+ * to avoid executing the source when importing the module.
+ */
 function wrapElmCode (code) {
   return `
     function wrapper() {
