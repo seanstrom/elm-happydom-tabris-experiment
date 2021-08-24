216b2a7ee39ffddbc31cce3df6032ae7479ecf14
Sean Hagstrom <sean@seanhagstrom.com>
2021-08-23 21:14:57 -0700

    add custom elm support to rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,20 +1,26 @@
+import path from 'path'
 import typescript from '@rollup/plugin-typescript'
+import elm from './elm-rollup-plugin'
 
 export default {
   input: 'src/index.jsx',
   output: {
-    dir: 'dist/',
+    file: 'dist/index.js',
     format: 'cjs',
     exports: 'auto',
-    preserveModules: true,
   },
-  plugins: [typescript({
-    tsconfig: './tsconfig.json',
-    include: [
-      "./src/**/*.ts",
-      "./src/**/*.tsx",
-      "./src/**/*.js",
-      "./src/**/*.jsx"
-    ]
-  })]
+  plugins: [
+    typescript({
+      tsconfig: './tsconfig.json',
+      include: [
+        "./src/**/*.ts",
+        "./src/**/*.tsx",
+        "./src/**/*.js",
+        "./src/**/*.jsx"
+      ]
+    }),
+    elm({
+      pathToElm: path.resolve(__dirname, 'node_modules/elm/bin/elm')
+    })
+  ]
 };
